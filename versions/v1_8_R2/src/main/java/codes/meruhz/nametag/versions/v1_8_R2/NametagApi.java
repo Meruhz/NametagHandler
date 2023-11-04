4f09f2da58d363b80252a003a25fe99c861f0fb7
Meruhz <meruhz.contato@gmail.com>
2023-11-04 14:22:30 -0300

    Finished v1_8_R2 implementation

---
+++ versions/v1_8_R2/src/main/java/codes/meruhz/nametag/versions/v1_8_R2/NametagApi.java
@@ -0,0 +1,43 @@
+package codes.meruhz.nametag.versions.v1_8_R2;
+
+import codes.meruhz.nametag.api.impl.AbstractNametagApi;
+import codes.meruhz.nametag.api.utils.ComponentUtils;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.minecraft.server.v1_8_R2.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class NametagApi extends AbstractNametagApi {
+
+    @Override
+    protected void setNametag(@NotNull UUID user, @NotNull ChatColor color, @NotNull BaseComponent @Nullable [] prefix, @NotNull BaseComponent @Nullable [] suffix, boolean visible, boolean collidable) {
+        @Nullable Player player = Bukkit.getPlayer(user);
+
+        if(player == null) {
+            throw new NullPointerException("User '" + user + "' is not online");
+        }
+
+        @NotNull Scoreboard scoreboard = new Scoreboard();
+        @Nullable ScoreboardTeam team = scoreboard.getTeam(player.getName());
+        team = team == null ? scoreboard.createTeam(player.getName()) : team;
+
+        team.a(EnumChatFormat.valueOf(color.name()));
+        team.setPrefix(prefix == null ? "" : ComponentUtils.getText(prefix));
+        team.setSuffix(suffix == null ? "" : ComponentUtils.getText(suffix));
+        team.setNameTagVisibility(visible ? ScoreboardTeamBase.EnumNameTagVisibility.ALWAYS : ScoreboardTeamBase.EnumNameTagVisibility.NEVER);
+        team.b(ScoreboardTeamBase.EnumNameTagVisibility.NEVER);
+
+        if(!scoreboard.addPlayerToTeam(player.getName(), team.getName())) {
+            throw new RuntimeException("An error occurred while add player '" + player.getName() + "' to team '" + team.getName() + "'");
+        }
+
+        @NotNull PacketPlayOutScoreboardTeam packet = new PacketPlayOutScoreboardTeam(team, 0);
+        Bukkit.getOnlinePlayers().forEach(target -> ((CraftPlayer) target).getHandle().playerConnection.sendPacket(packet));
+    }
+}
